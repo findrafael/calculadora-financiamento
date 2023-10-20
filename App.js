96c9fe0b9a92cae03caea10ccc6c788198a0f9f2
Rafael Luna <newsadnessboy@gmail.com>
2023-10-20 19:51:42 -0300

    adicao de tratativa de erro + componentizacao basica

--- App.js
+++ App.js
@@ -1,48 +1,11 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Calculadora from "./components/calculadora.js";
 
 const App = () => {
-  const [valorInicial, setValorInicial] = useState("");
-  const [taxaJuros, setTaxaJuros] = useState("");
-  const [periodoMeses, setPeriodoMeses] = useState("");
-  const [resultado, setResultado] = useState("");
-
-  const calcular = () => {
-    const taxaJurosDecimal = parseInt(taxaJuros) / 100;
-    const auxiliar1 = ((1 + taxaJurosDecimal) ** periodoMeses) * taxaJurosDecimal;
-    const auxiliar2 = ((1 + taxaJurosDecimal) ** periodoMeses) - 1;
-    const parcela = ((valorInicial * auxiliar1) / auxiliar2).toFixed(2);
-    setResultado(parcela);
-  };
-
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Calculadora Financeira</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Valor a ser financiado"
-        keyboardType="numeric"
-        onChangeText={(text) => setValorInicial(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Taxa de Juros x% a.m."
-        keyboardType="numeric"
-        onChangeText={(text) => setTaxaJuros(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Período em meses"
-        keyboardType="numeric"
-        onChangeText={(text) => setPeriodoMeses(text)}
-      />
-      <Button
-        title="Calcular"
-        onPress={calcular}
-        style={styles.botao}
-      />
-      <Text style={styles.resultado}>O valor da sua parcela, será de:</Text>
-      <Text style={styles.resultado}>{resultado}</Text>
+      <Calculadora />
     </View>
   );
 };
@@ -50,30 +13,6 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  titulo: {
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-  input: {
-    width: 200,
-    height: 40,
-    borderColor: "#000",
-    borderWidth: 1,
-    padding: 10,
-  },
-  botao: {
-    width: 200,
-    height: 40,
-    backgroundColor: "#fcba03",
-    color: "#fcba03",
-    borderRadius: 5,
-  },
-  resultado: {
-    fontSize: 20,
-    fontWeight: "bold",
   },
 });
 
